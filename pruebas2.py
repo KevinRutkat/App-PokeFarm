62f264fd368832491c8d279231ce7deb20277bc1
Kevin <1926157@alu.murciaeduca.es>
2025-05-27 18:29:08 +0200

    Eliminar archivos de pruebas

--- pruebas2.py
+++
@@ -1,7 +0,0 @@
-import pygetwindow as gw
-
-# Obtener y mostrar todas las ventanas abiertas
-ventanas = gw.getAllTitles()
-
-for ventana in ventanas:
-    print(ventana)  # Imprime los nombres de todas las ventanas abiertas
